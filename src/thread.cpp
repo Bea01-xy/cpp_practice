dc64017e5fa65a6193c2827759de577a5f88826d
Bea01-xy <1342806556@qq.com>
2026-02-23 21:11:21 +0800

    added a new line in signal.cpp

--- src/thread.cpp
+++ src/thread.cpp
@@ -18,14 +18,14 @@ public:
     }
 };
 
-class PrintHelloPer300ms
+class PrintHelloPer200ms
 {
 public:
     void operator()(std::string name)
     {
         for (size_t i = 0; i < 5; i++) {
             std::cout << "Hello, " << name << std::endl;
-            std::this_thread::sleep_for(300ms);
+            std::this_thread::sleep_for(200ms);
         }
     }
 };
@@ -40,7 +40,7 @@ void PrintHelloPerSecond(std::string name)
 
 int main (int argc, char *argv[]) {
     std::thread t1(PrintHelloPer500ms(), "Bea");
-    std::thread t2(PrintHelloPer300ms(), "Lamino");
+    std::thread t2(PrintHelloPer200ms(), "Lamino");
     std::thread t3(PrintHelloPerSecond, "Suisei");
     std::cout << "This is from main function." << std::endl;
     t1.join();
